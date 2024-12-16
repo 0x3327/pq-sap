d8c13adcbe791b61cf6ebf155710136aca352d9d
strahinjapraska <strahinja.praska6@gmail.com>
2024-12-16 16:35:26 +0100

    Cargo and build

---
+++ build.rs
@@ -0,0 +1,15 @@
+extern crate cc; 
+
+fn main(){
+    cc::Build::new()
+    .file("src/newhope/ref/ccakem.c")
+    .file("src/newhope/ref/cpapke.c")
+    .file("src/newhope/ref/poly.c")
+    .file("src/newhope/ref/ntt.c")
+    .file("src/newhope/ref/randombytes.c")
+    .file("src/newhope/ref/precomp.c")
+    .file("src/newhope/ref/fips202.c")
+    .file("src/newhope/ref/reduce.c") 
+    .file("src/newhope/ref/verify.c")
+    .compile("ccakem");
+}
